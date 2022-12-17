56878d11f7c012f404229372e9f9fece746b3881
FredGagne79 <120513421+FredGagne79@users.noreply.github.com>
2022-12-17 12:34:22 -0500

    Add files via upload

---
+++ detection.ps1
@@ -0,0 +1,12 @@
+﻿$Path = Test-Path -Path "C:\Program Files\Learnpulse\Screenpresso\activation.lic"
+$Path1 = Test-Path -Path "C:\Program Files\Learnpulse\Screenpresso"
+
+If ($Path1 -eq $True -and $Path -eq $False) {
+    Write-warning "Licence Required"
+    Exit 1
+}
+else {
+    Write-Output "Not required"
+    Exit 0
+}
+

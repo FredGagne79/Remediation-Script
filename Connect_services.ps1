22ca258c206ba2930f2e14a0ea2c0e2847c5d8cc
FredGagne79 <120513421+FredGagne79@users.noreply.github.com>
2025-03-08 10:01:46 -0500

    Add files via upload

---
+++ Connect_services.ps1
@@ -0,0 +1,20 @@
+﻿
+#from time to time, make sure to run "update-Module"
+
+
+#if the exchange online module is misbehaving
+#Uninstall-Module -Name ExchangeOnlineManagement -AllVersions -Force
+#Install-Module -Name ExchangeOnlineManagement -RequiredVersion 3.5.1 -Force
+
+#update-Module
+set-executionpolicy remotesigned
+Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All","MailboxSettings.Read","User.Readwrite.All" -NoWelcome
+Connect-MicrosoftTeams
+Connect-MsolService
+Connect-AzureAD
+Connect-ExchangeOnline -UserPrincipalName adm.fgagne@flo.com
+
+
+
+
+

0b69f9e1822f836bcdf55a0c7b473227663807b9
FredGagne79 <120513421+FredGagne79@users.noreply.github.com>
2025-03-08 10:03:08 -0500

    Add files via upload

---
+++ install_service.ps1
@@ -0,0 +1,19 @@
+﻿get-Module -Name ImportExcel
+
+Install-Module Microsoft.Graph -Scope AllUsers
+
+Install-Module Microsoft.Graph -Scope CurrentUser
+
+Install-Module ExchangeOnlineManagement -force
+
+Install-Module -Name AzureAD -Force -AllowClobber -Scope CurrentUser
+
+Import-Module AzureAD -Force
+
+get-Module ExchangeOnlineManagement
+
+update-Module ExchangeOnlineManagement -force
+
+install-module microsoftteams
+
+install-module MSOnline
